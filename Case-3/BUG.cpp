e6b7565db51b8b55a20025438574f740e28a8144
Daniel <danielbebon@gmail.com>
2021-10-14 22:54:28 -0500

    Create BUG.cpp

---
+++ Case-3/BUG.cpp
@@ -0,0 +1,32 @@
+/*Programa que pueda hacer suma,resta y multiplicacion de valores acumulados*/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	char letra;
+	int cont,i,n,numeros[n],suma=0,multiplicacion,hel;	/*hel variable que ayudara a completar la multiplicacion*/
+	cout<<"\tBienvenido"<<endl;
+	cout<<"Digite cuantos numeros utilizara: "<<endl;cin>>n;
+	i=0,cont=1,hel=1;
+	while(i < n){
+		cout<<"Digite el numero "<<cont<<": "<<endl;cin>>numeros[i];
+		i+=1,cont++;
+	}
+	cout<<"Digite A para realizar SUMA - Digite B para realizar MULTIPLICACION: "<<endl;cin>>letra;
+	switch(letra){
+		case 'A':
+ 		    for(int cont2=0;cont2<=n;cont2++){
+				suma+=numeros[cont2];
+			}
+		cout<<"La suma de sus numeros es: "<<suma;break;
+		case 'B':
+			for(int cont2=0;cont<=n;cont2++){
+				multiplicacion=numeros[cont2]*numeros[hel];
+				hel=multiplicacion;
+			}
+		cout<<"La multiplicacion de sus numeros es: "<<multiplicacion;break;
+		default:
+			cout<<"Usted ha digitado un numero NO REGISTRADO, vuelva a intentarlo";break;
+	}
+	return 0;
+}
