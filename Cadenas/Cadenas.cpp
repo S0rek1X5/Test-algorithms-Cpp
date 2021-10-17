75b7c91de3b1839ba50f3ae04fba4abf464c1e8e
Daniel <danielbebon@gmail.com>
2021-10-17 18:15:01 -0500

    Update Cadenas.cpp

--- Cadenas/Cadenas.cpp
+++ Cadenas/Cadenas.cpp
@@ -7,7 +7,11 @@ int main(){
 	cout<<"Digite su nombre y apellido: ";cin.getline(texto,100);
 	cout<<"Digite su otro nombre y apellido: ";cin.getline(nombre,100);
 	strcpy(texto2,texto);  //Funcion strCPY  =  Copiar texto a otro
+	strcat(texto,nombre);  //Funcion Para juntar cadenas 
 	tamanho=strlen(texto);  //Funcion strLEN  =  Determinar la cantidad de letras
+
+	/*
+	cout<<texto<<endl;
 	if(strcmp(texto,nombre) == 0){     //Funcion strCMP  =  Hace comparaciones entre cadenas
 		cout<<"Ambos son iguales"<<endl;
 	}
@@ -24,7 +28,8 @@ int main(){
 	else if(strcmp(nombre,texto)>0){
 		cout<<nombre<<" Es mayor alfabeticamente que "<<texto<<endl;
 	}
-	
+	*/
+	/*
 	//Tamanho
 	if(tamanho>10){
 		cout<<texto2<<" Supera los 10 digitos";
@@ -32,5 +37,6 @@ int main(){
 	else{
 		cout<<"ERROR";
 	}
+	*/
 	return 0;
 }
