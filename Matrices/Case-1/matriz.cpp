048643dd8a83261f2c387127697fa1e61c7e162c
Daniel <danielbebon@gmail.com>
2021-10-16 22:01:37 -0500

    Update matriz.cpp

--- Matrices/Case-1/matriz.cpp
+++ Matrices/Case-1/matriz.cpp
@@ -3,6 +3,22 @@ using namespace std;
 int main(){
 	int matriz1[3][3]={{1,2,3},{4,5,6},{3,5,2}},matriz2[3][3]={{5,4,3},{2,1,0},{3,1,4}};
 	int matriz3[3][3];
+	cout<<"Sus matrices son:"<<endl;
+	cout<<"Matriz 1"<<endl;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cout<<" "<<matriz1[i][j];
+		}
+		cout<<endl;
+	}
+	cout<<"Matriz 2"<<endl;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cout<<" "<<matriz2[i][j];
+		}
+		cout<<endl;
+	}
+	cout<<"\nLa suma de sus matrices es: \n";
 	for(int i=0;i<3;i++){
 		for(int j=0;j<3;j++){
 			matriz3[i][j]=matriz1[i][j]+matriz2[i][j];
@@ -10,7 +26,7 @@ int main(){
 	}
 	for(int i=0;i<3;i++){
 		for(int j=0;j<3;j++){
-			cout<<matriz3[i][j];
+			cout<<" "<<matriz3[i][j];
 		}
 		cout<<endl;
 	}
